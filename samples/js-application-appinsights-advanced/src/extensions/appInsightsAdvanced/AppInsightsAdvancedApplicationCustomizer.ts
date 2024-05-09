1784b4f360ff1347c65a586e1608c3474727baf1
Derek Smay <43035577+desmay@users.noreply.github.com>
2024-05-09 13:46:38 +0000

    conn string

--- samples/js-application-appinsights-advanced/src/extensions/appInsightsAdvanced/AppInsightsAdvancedApplicationCustomizer.ts
+++ samples/js-application-appinsights-advanced/src/extensions/appInsightsAdvanced/AppInsightsAdvancedApplicationCustomizer.ts
@@ -32,7 +32,7 @@ export default class AppInsightsAdvancedApplicationCustomizer
         var appInsights = new ApplicationInsights({
             config: {
                 maxBatchInterval: 0,
-                connectionString:'InstrumentationKey=ec8c1c57-6733-f194-8753-a4b309fd7c82;EndpointSuffix=applicationinsights.us;IngestionEndpoint=https://usgovvirginia-1.in.applicationinsights.azure.us/;AADAudience=https://monitor.azure.us/;ApplicationId=304a6534-3e67-4af8-a6b7-5613cac9066a',
+                connectionString:'InstrumentationKey=74134282-0275-f4b5-9d85-80e665c7ca1d;EndpointSuffix=applicationinsights.us;IngestionEndpoint=https://usgovvirginia-1.in.applicationinsights.azure.us/;AADAudience=https://monitor.azure.us/;ApplicationId=275ff6ae-ecb1-4bad-8948-32623ba15e82',
                 disableFetchTracking: true,
                 disableAjaxTracking: true,
                 //enableAutoRouteTracking: true,
